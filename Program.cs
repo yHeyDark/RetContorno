d160edbc8dcce89f3f24a82717148bef5eeeca48
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-06-02 23:09:44 -0300

    Retangulopersonalizado

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿int largura, altura;
+
+Console.WriteLine("Tamanho do Triangulo");
+
+Console.Write("Largura(1 a 10): ");
+if (!int.TryParse(Console.ReadLine(), out largura) || largura < 1 || largura > 10)
+{
+    Console.WriteLine("largura invalida digite um número de 1 a 10");
+    return;
+}
+
+Console.Write("Altura(1 a 10): ");
+if (!int.TryParse(Console.ReadLine(), out altura) || altura < 1 || largura > 10)
+{
+    Console.WriteLine("Altura invalida digite um número de 1 a 10");
+    return;
+}
+
+for (int i = 0; i < altura; i++)
+{
+    for (int j = 0; j < largura; j++)
+    {
+        if (i == 0 || i == altura - 1 || j == 0 || j == largura - 1)
+        {
+            Console.Write("*");
+        }
+
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+    }
